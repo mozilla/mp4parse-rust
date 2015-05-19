47d39bcf6a191c925136455e30c22185659b4ed3
Ralph Giles <giles@mozilla.com>
2015-05-19 16:51:28 -0700

    Move dump code into its own function.

--- examples/dump.rs
+++ examples/dump.rs
@@ -3,11 +3,16 @@ extern crate mp4parse;
 use std::fs::File;
 use std::io::{Seek,SeekFrom};
 
-fn main() {
-    let mut f = File::open("test.mp4").unwrap();
+fn dump_file(filename: String) {
+    let mut f = File::open(filename).unwrap();
     let h = mp4parse::read_box_header(&mut f).unwrap();
     println!("{}", h);
     f.seek(SeekFrom::Current(h.size as i64 - 8)).unwrap();
     let h = mp4parse::read_box_header(&mut f).unwrap();
     println!("{}", h);
+}
+
+fn main() {
+    let filename = "test.mp4".to_string();
+    dump_file(filename);
 }
