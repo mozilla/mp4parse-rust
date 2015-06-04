cd0be9b105b5eb2461b177b60df20bc5691c9d1f
Ralph Giles <giles@mozilla.com>
2015-06-03 17:08:27 -0700

    Demonstrate recursion for content parsing in dump.rs.
    On successful header parse, we match on the box type.
    Although PartialEq is implemented between String and &str,
    match doesn't seem to know how to convert if I just do
    match a_string { "foo" => {}, ... } so instead we match
    on a reference to a slice containing the whole string
    contents.
    
    If the box type is 'ftyp' we wrap the reader in a Take
    so we can't read past the declared content size, and
    pass that to the content reader function.
    
    Otherwise, we skip the contents as before. There's a
    problem with recursing here though: std::io::Seek
    isn't implemented for Take<&mut File> so we can't
    pass &mut content to anything which calls
    skip_box_contents(). Not sure what to do there.

--- examples/dump.rs
+++ examples/dump.rs
@@ -2,7 +2,12 @@ extern crate mp4parse;
 
 use std::env;
 use std::fs::File;
+use std::io::{Read, Take};
 use std::thread;
+
+fn limit<'a>(f: &'a mut File, h: &mp4parse::BoxHeader) -> Take<&'a mut File> {
+    f.take(h.size - h.offset)
+}
 
 fn dump_file(filename: String) {
     let mut f = File::open(filename).unwrap();
@@ -10,10 +15,18 @@ fn dump_file(filename: String) {
         loop {
             match mp4parse::read_box_header(&mut f) {
                 Some(h) => {
-                    println!("{}", h);
-                    mp4parse::skip_box_content(&mut f, &h).unwrap();
+                    match &(mp4parse::fourcc_to_string(h.name))[..] {
+                        "ftyp" => {
+                            let mut content = limit(&mut f, &h);
+                            println!("{}", mp4parse::read_ftyp(&mut content, &h).unwrap())
+                        },
+                        _ => {
+                            println!("{}", h);
+                            mp4parse::skip_box_content(&mut f, &h).unwrap();
+                        },
+                    }
                 },
-                _ => break,
+                None => break,
             }
         }
     });
