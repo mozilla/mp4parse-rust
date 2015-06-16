d7454e497c48d7c5c536dc339cbbffa9650102d7
Ralph Giles <giles@mozilla.com>
2015-06-15 17:50:36 -0700

    Remove task wrapper from the dump example.
    Until we actually handle the failure, this isn't very helpful.

--- examples/dump.rs
+++ examples/dump.rs
@@ -2,17 +2,12 @@ extern crate mp4parse;
 
 use std::env;
 use std::fs::File;
-use std::thread;
 
 fn dump_file(filename: String) {
     let mut f = File::open(filename).unwrap();
-    let task = thread::spawn(move || {
-        loop {
-            mp4parse::read_box(&mut f);
-        }
-    });
-    // Catch and ignore any panics in the thread.
-    task.join().ok();
+    loop {
+        mp4parse::read_box(&mut f).unwrap();
+    }
 }
 
 fn main() {
