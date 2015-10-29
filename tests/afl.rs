b584de0de5d43ca85c0b47599a3d7faef263290c
Ralph Giles <giles@mozilla.com>
2015-10-29 11:49:45 -0700

    Document what each afl fuzz case does.

--- tests/afl.rs
+++ tests/afl.rs
@@ -1,14 +1,18 @@
-// Regression tests from American Fuzzy Lop test cases.
+/// Regression tests from American Fuzzy Lop test cases.
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+/// These all caused crashes at some point during development.
 
 extern crate mp4parse;
 
 use std::io::Cursor;
 
 /// https://github.com/mozilla/mp4parse-rust/issues/2
+///
+/// Test a box with 4-byte size, smaller than the smallest header.
 #[test]
 fn fuzz_2() {
     let mut c = Cursor::new(b"\x00\x00\x00\x04\xa6\x00\x04\xa6".to_vec());
@@ -17,6 +21,8 @@ fn fuzz_2() {
 }
 
 /// https://github.com/mozilla/mp4parse-rust/issues/4
+///
+/// Test a large (64 bit) box header with zero declared size.
 #[test]
 fn fuzz_4() {
     let mut c = Cursor::new(b"\x00\x00\x00\x01\x30\x30\x30\x30\x00\x00\x00\x00\x00\x00\x00\x00".to_vec());
@@ -25,6 +31,9 @@ fn fuzz_4() {
 }
 
 /// https://github.com/mozilla/mp4parse-rust/issues/5
+///
+/// Declares 202116104 compatible brands but does not supply them,
+/// verifying read is properly bounded at the end of the stream.
 #[test]
 fn fuzz_5() {
     let mut c =
