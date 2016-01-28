ab29235aef483c38d188df5b78c70ae56897b3bc
Ralph Giles <giles@mozilla.com>
2016-01-28 13:40:35 -0800

    capi: Invoke examples/Makefile as part of the standard build.
    This is pretty brittle, relying on cargo's debug build of
    the byteorder crate. Nevertheless, we want to know when
    we break header generation.

--- build.rs
+++ build.rs
@@ -1,10 +1,21 @@
 extern crate cheddar;
 
 fn main() {
+    // Generate mp4parse.h.
     cheddar::Cheddar::new().expect("could not read manifest")
         .module("capi").expect("invalid module path")
         .insert_code("// This Source Code Form is subject to the terms of the Mozilla Public\n")
         .insert_code("// License, v. 2.0. If a copy of the MPL was not distributed with this\n")
         .insert_code("// file, You can obtain one at https://mozilla.org/MPL/2.0/.")
         .run_build("include/mp4parse.h");
+
+    // Try compiling it.
+    let job = std::process::Command::new("sh")
+        .arg("-c")
+        .arg("make -C examples")
+        .output()
+        .unwrap_or_else(|e| { panic!("Failed to invoke makefile: {}", e); });
+    for line in job.stdout {
+        println!("{}", line);
+    }
 }
