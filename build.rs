431c8f7a12386679210ad1f3e60e6900318178db
Matthew Gregan <kinetik@flim.org>
2016-01-29 17:37:09 +1300

    Move try-compile of examples to a Travis rule for now.
    With the try-compile stage in build.rs, attempting to compile when errors
    are present (common during development) results in the try-compile failing
    with a less-than-useful error and the main cargo build not running at all,
    obscuring the compiler errors causing the try-compile to fail.
    
    Until a better solution presents itself, move the try-compile to a Travis
    rule to retain some CI of header generation.  And try to remember to run
    'make -C examples' locally when making capi.rs changes.

--- build.rs
+++ build.rs
@@ -8,12 +8,4 @@ fn main() {
         .insert_code("// License, v. 2.0. If a copy of the MPL was not distributed with this\n")
         .insert_code("// file, You can obtain one at https://mozilla.org/MPL/2.0/.")
         .run_build("include/mp4parse.h");
-
-    // Try compiling it.
-    let job = std::process::Command::new("/bin/sh")
-        .arg("-c")
-        .arg("make -C examples")
-        .status()
-        .expect("error: Unable to invoke makefile.");
-    assert!(job.success(), "error: C++ test build failed.");
 }
