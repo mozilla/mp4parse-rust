c5580319a70d351a0365cde5202254c4d28a2061
alfredoyang <ayang@mozilla.com>
2016-10-08 20:08:06 +0800

    Parse quicktime audio sample entry format.
    Quicktime uses a 'wave' atom in audio sample entry box. For 'mp4a', its
    'esds' is inside 'wave' atom. That is:
    mp4a => {
        wave => {
            esds => {
            }
        }
    }
    
    Detail spec can be found at: http://multimedia.cx/mirror/qtff-2007-09-04.pdf

--- mp4parse/src/boxes.rs
+++ mp4parse/src/boxes.rs
@@ -58,4 +58,5 @@ box_database!(
     ProtectedAudioSampleEntry  0x656e6361, // "enca" - Need to check official name in spec.
     MovieExtendsBox            0x6d766578, // "mvex"
     MovieExtendsHeaderBox      0x6d656864, // "mehd"
+    QTWaveAtom                 0x77617665, // "wave" - quicktime atom
 );
