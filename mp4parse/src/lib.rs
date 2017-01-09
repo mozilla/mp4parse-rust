a18a06e9ed3e444b819fd55dce4776592a2074f1
AlfredoYang <ayang@mozilla.com>
2017-01-09 17:11:03 +0800

    Add testcase and use bool instead of u8 while reading 1 bit.

--- mp4parse/src/lib.rs
+++ mp4parse/src/lib.rs
@@ -1311,45 +1311,26 @@ fn read_ds_descriptor(data: &[u8], esds: &mut ES_Descriptor) -> Result<()> {
         bit_reader.skip(3)?;    // num_assoc_data
         bit_reader.skip(4)?;    // num_valid_cc
 
-        let mono_mixdown_present: u8 = ReadInto::read(bit_reader, 1)?;
-        if mono_mixdown_present > 0 {
+        let mono_mixdown_present: bool = ReadInto::read(bit_reader, 1)?;
+        if mono_mixdown_present {
             bit_reader.skip(4)?;    // mono_mixdown_element_number
         }
 
-        let stereo_mixdown_present: u8 = ReadInto::read(bit_reader, 1)?;
-        if stereo_mixdown_present > 0 {
+        let stereo_mixdown_present: bool = ReadInto::read(bit_reader, 1)?;
+        if stereo_mixdown_present {
             bit_reader.skip(4)?;    // stereo_mixdown_element_number
         }
 
-        let matrix_mixdown_idx_present: u8 = ReadInto::read(bit_reader, 1)?;
-        if matrix_mixdown_idx_present > 0 {
+        let matrix_mixdown_idx_present: bool = ReadInto::read(bit_reader, 1)?;
+        if matrix_mixdown_idx_present {
             bit_reader.skip(2)?;    // matrix_mixdown_idx
             bit_reader.skip(1)?;    // pseudo_surround_enable
         }
 
-        for _ in 0..num_front_channel {
-            let front_element_is_cpe: u8 = ReadInto::read(bit_reader, 1)?;
-            channel_counts += if front_element_is_cpe > 0 { 2 } else { 1 };
-            bit_reader.skip(4)?;    // front_element_tag_select
-        }
-
-        for _ in 0..num_side_channel {
-            let side_element_is_cpe: u8 = ReadInto::read(bit_reader, 1)?;
-            channel_counts += if side_element_is_cpe > 0 { 2 } else { 1 };
-            bit_reader.skip(4)?;    // side_element_tag_select
-        }
-
-        for _ in 0..num_back_channel {
-            let back_element_is_cpe: u8 = ReadInto::read(bit_reader, 1)?;
-            channel_counts += if back_element_is_cpe > 0 { 2 } else { 1 };
-            bit_reader.skip(4)?;    // back_element_tag_select
-        }
-
-        for _ in 0..num_lfe_channel {
-            let lft_element_is_cpe: u8 = ReadInto::read(bit_reader, 1)?;
-            channel_counts += if lft_element_is_cpe > 0 { 2 } else { 1 };
-            bit_reader.skip(4)?;    // lft_element_tag_select
-        }
+        channel_counts += read_surround_channel_count(bit_reader, num_front_channel)?;
+        channel_counts += read_surround_channel_count(bit_reader, num_side_channel)?;
+        channel_counts += read_surround_channel_count(bit_reader, num_back_channel)?;
+        channel_counts += read_surround_channel_count(bit_reader, num_lfe_channel)?;
     }
 
     esds.audio_object_type = Some(audio_object_type);
@@ -1357,6 +1338,16 @@ fn read_ds_descriptor(data: &[u8], esds: &mut ES_Descriptor) -> Result<()> {
     esds.audio_channel_count = Some(channel_counts);
 
     Ok(())
+}
+
+fn read_surround_channel_count(bit_reader: &mut BitReader, channels: u8) -> Result<u16> {
+    let mut count = 0;
+    for _ in 0..channels {
+        let is_cpe: bool = ReadInto::read(bit_reader, 1)?;
+        count += if is_cpe { 2 } else { 1 };
+        bit_reader.skip(4)?;
+    }
+    Ok(count)
 }
 
 fn read_dc_descriptor(data: &[u8], esds: &mut ES_Descriptor) -> Result<()> {
