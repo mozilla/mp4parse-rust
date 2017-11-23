268c5b8e60fa9a5c3a67f5ce8d553b3802390809
Corey Farwell <coreyf@rwell.org>
2017-11-23 11:50:44 -0800

    Bump afl.rs integration to reflect latest changes.
    https://users.rust-lang.org/t/announcing-afl-rs-0-2-bindings-for-american-fuzzy-lop/13981
    
    afl.rs no longer requires unstable Rust and also doesn't need a compiler
    plugin.

--- mp4parse/src/lib.rs
+++ mp4parse/src/lib.rs
@@ -3,8 +3,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
-#![cfg_attr(feature = "fuzz", feature(plugin))]
-#![cfg_attr(feature = "fuzz", plugin(afl_plugin))]
 #[cfg(feature = "fuzz")]
 extern crate afl;
 
