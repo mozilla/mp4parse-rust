1666db743a97f6d58b24f6d314f6a87297c3ae75
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2020-03-02 17:34:43 -0800

    Remove afl fuzzing code
    All fuzzing is now handled by libfuzzer_sys

--- mp4parse/src/lib.rs
+++ mp4parse/src/lib.rs
@@ -3,8 +3,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
-#[cfg(feature = "fuzz")]
-extern crate afl;
 
 #[macro_use]
 extern crate log;
