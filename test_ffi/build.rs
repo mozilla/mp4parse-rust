62b1d4c8a2dfa490f61c03343ebe957663c9dedb
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2020-05-05 12:23:11 -0700

    Update mp4parse_capi for standard mozilla vendoring
    - Remove build.rs in favor of executing cbindgen externally
    - Remove cbindgen build dependency
    - Add cbindgen.toml for generated bindings preferences
    
    And corresponding changes to test_ffi:
    - Generate mp4parse_capi header in build.rs
    - Configure cbindgen via cbindgen.toml
    - Put generated code in out OUT_DIR
    - Include generated code indirectly, as mozilla tree does
    
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1624056

--- test_ffi/build.rs
+++ test_ffi/build.rs
@@ -1,6 +1,22 @@
+extern crate cbindgen;
 extern crate cc;
 
+use cbindgen::{Config, RenameRule};
+use std::path::Path;
+
+const CAPI_CRATE: &str = "../mp4parse_capi";
+
 fn main() {
+    println!("cargo:rerun-if-changed={}/src/lib.rs", CAPI_CRATE);
+
+    let crate_dir = Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join(CAPI_CRATE);
+    let generated_include_dir = Path::new(&std::env::var("OUT_DIR").unwrap()).join("include");
+    let header_path = generated_include_dir.join("mp4parse_ffi_generated.h");
+
+    cbindgen::generate(&crate_dir)
+        .expect("Could not generate header")
+        .write_to_file(header_path);
+
     println!("cargo:rerun-if-changed=src/main.rs");
     println!("cargo:rerun-if-changed=src/test.cc");
 
@@ -8,7 +24,8 @@ fn main() {
         .file("src/test.cc")
         .cpp(true)
         .flag_if_supported("-std=c++11")
-        .include("../mp4parse_capi/include")
+        .include("include")
+        .include(generated_include_dir)
         .compile("libtest.a");
 
     #[cfg(unix)]
