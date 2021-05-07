bc0f5a75b6c9c672f0ffd9305b00680c2375967d
Adrian <adrian@vailacom.net>
2021-05-07 14:26:02 +0200

    Fix clippy warnings

--- mp4parse/src/unstable.rs
+++ mp4parse/src/unstable.rs
@@ -498,7 +498,7 @@ pub struct Microseconds<T>(pub T);
 pub fn media_time_to_us(time: MediaScaledTime, scale: MediaTimeScale) -> Option<Microseconds<u64>> {
     let microseconds_per_second = 1_000_000;
     rational_scale::<u64, u64>(time.0, scale.0, microseconds_per_second)
-        .and_then(|v| Some(Microseconds(v)))
+        .map(|v| Microseconds(v))
 }
 
 /// Convert `time` in track's local (mdhd) timescale to microseconds,
@@ -513,7 +513,7 @@ where
     assert_eq!(time.1, scale.1);
     let microseconds_per_second = 1_000_000;
     rational_scale::<T, u64>(time.0, scale.0, microseconds_per_second)
-        .and_then(|v| Some(Microseconds(v)))
+        .map(|v| Microseconds(v))
 }
 
 #[test]
